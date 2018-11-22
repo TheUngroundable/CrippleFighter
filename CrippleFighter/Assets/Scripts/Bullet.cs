ad4ccf90d73cb2bb7f856cf6c38579a075410cc7
TheUngroundable <35199075+TheUngroundable@users.noreply.github.com>
2018-11-22 16:14:23 +0100

    Initialized with Unity

---
+++ CrippleFighter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public GameObject owner;
+    public int damage = 1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+       
+        if (col.gameObject.tag == "Player" && col.gameObject!=owner)
+        {
+
+            col.gameObject.GetComponent<Player>().health-=damage;
+
+        }
+    }
+
+}
---
+++ CrippleFighter/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    public int health = 3;
+
+    // Use this for initialization
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if (Input.GetButtonUp("Fire1")){
+
+            Fire();
+        
+        }
+
+        if (health<=0){
+
+            Die();
+
+        }
+
+    }
+
+    void Die(){
+
+        Destroy(this.gameObject);
+
+    }
+
+    void Fire(){
+
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+        bulletPrefab,
+        bulletSpawn.position,
+        bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        bullet.GetComponent<Bullet>().owner = this.gameObject;
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 10.0f);
+
+    }
+}
